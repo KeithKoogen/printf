adb131238a9a334aca70ae19044dce2eac4bd5fb
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-15 23:13:27 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -6,6 +6,7 @@
 /**
  * print_number - prints numbers
  * @n: amount of numbers
+ * @count: a pointer to an int that keeps count
  * Return: nothing
  */
 
@@ -14,18 +15,16 @@ void print_numbers(int n, unsigned int *count)
 	if (n < 10)
 	{
 		printc(n + 48, count);
-		
 		return;
 	}
 	print_numbers(n/10, count);
 	printc((n % 10) + 48, count);
-
-    
 }
 
 /**
  * print_char - prints a char
  * @ptr: pointer
+ * @count: a pointer to an int that keeps count
  * @va_list: type for iterating arguments
  * Return: nothing
  */
@@ -35,12 +34,12 @@ void print_char(va_list *ptr, unsigned int *count)
   int i;
   i = va_arg(*ptr, int);
  printc(i, count);
-
 }
 
 /**
  * print_integer - prints an integer
  * @ptr: pointer
+ * @count: a pointer to an int that keeps count
  * @va_list: type for iterating arguments
  * Return: nothing
  */
@@ -52,21 +51,18 @@ void print_integer(va_list *ptr, unsigned int *count)
 	if (i > -1)
 	{
 		print_numbers(i, count);
-	
 	}
 	else 
 	{
-		
 		printc('-', count);
 		print_numbers(-i, count);
-		
 	}
- 
 }
 
 /**
  * print_string - prints a string
  * @ptr: pointer
+ * @count: a pointer to an int that keeps count
  * @va_list: type for iterating arguments
  * Return: nothing
  */
@@ -78,7 +74,7 @@ void print_string(va_list *ptr, unsigned int *count)
 	
   i = 0;
   str = va_arg(*ptr, char *);
-	
+
   while (str[i] != '\0')
   {
   	printc(str[i], count);
@@ -88,44 +84,40 @@ void print_string(va_list *ptr, unsigned int *count)
 }
 /**
  * _printf - a function that prints
- * @char: character
  * @format: type of data to be printed on standard output
  * Return: int
  */
 
 int _printf(const char *format, ...)
 {
-  function functions[] = {
-    {'c', print_char},
-    {'s', print_string},
-    {'i', print_integer},
+	function functions[] = {
+	{'c', print_char},
+	{'s', print_string},
+	{'i', print_integer},
 	{'d', print_integer}
-  };
+	};
 	unsigned int i, k, *count, finalcount;
   va_list args, *ptr;
-	
+
 	if (format == NULL)
 		return (-1);
 
 	count = malloc(sizeof(int));
-	
+
 	if (count == NULL)
 		return (-1);
-	
+
 	ptr = &args;
 	if (ptr == NULL)
 		return (-1);
 	*count = 0;
 	
-
 	va_start(args, format);
 	while (*format != '\0')
 	{
 		if (*format != '%')
 		{
 			printc(*format, count);
-			
-			
 		}
 		else
 		{
@@ -133,17 +125,11 @@ int _printf(const char *format, ...)
 			k = 0;
 		while (i < 4)
 		{
-			
 				if (functions[i].symbol == *(format + 1))
 				{
 					functions[i].function(ptr, count);
-					
-					
 					++k;
-	
 				}
-					
-					
 			++i;
 		}
 			if (*(format + 1) == '%')
@@ -151,7 +137,7 @@ int _printf(const char *format, ...)
 				printc(*format, count);
 				++k;
 			}
-					
+
 			if (k == 1)
 			{
 				++format;
@@ -159,17 +145,15 @@ int _printf(const char *format, ...)
 			else
 			{
 				printc(*format, count);
-				
-			
 			}
 		}
 		++format;
 	}
-		
+
 	va_end(args);
-	
+
 	finalcount = *count;
 	free(count);
-  
+
 return (finalcount);
 }
