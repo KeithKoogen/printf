116f346f2d778641a15349dcbe1c2051fe70a408
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-15 21:00:52 +0200

    Update printc.c

--- printc.c
+++ printc.c
@@ -3,12 +3,13 @@
 /**
  * printc - writes the character c to stdout
  * @c: The character to print
+ * @count: keeps count of charcaters printed
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int printc(char c, unsigned int *ptr)
+int printc(char c, unsigned int *count)
 {
-	*ptr = *ptr + 1;
+	*count = *count + 1;
 	return (write(1, &c, 1));
 }
