64c3001afccf8e3eef7eb701a0963740b5fb1288
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-15 21:12:42 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -105,12 +105,12 @@ int _printf(const char *format, ...)
   va_list args, *ptr;
 	
 	if (format == NULL)
-		return (0);
+		return (NULL);
 
 	count = malloc(sizeof(int));
 	
 	if (count == NULL)
-		return (0);
+		return (NULL);
 	
 	ptr = &args;
 	*count = 0;
