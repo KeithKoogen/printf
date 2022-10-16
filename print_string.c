d58167bd9d5548bdf779f29e93c0ea5c28275459
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-10-16 22:26:52 +0200

    Create print_string.c

---
+++ print_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_string - prints a string
+ * @ptr: pointer
+ * @count: a pointer to an int that keeps count
+ * Return: nothing
+ */
+
+void print_string(va_list *ptr, unsigned int *count)
+{
+	char *str;
+	int i;
+
+  if (0 > i < 32 || 32 >= 127)
+  {
+    printc("\x", 41, 5a);
+  }
+   
+	i = 0;
+	str = va_arg(*ptr, char *);
+
+	while (str[i] != '\0')
+	{
+		printc(str[i], count);
+		++i;
+	}
+}
