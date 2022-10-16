bbb809cb262adb96e73ef069c1ffde1579a027bf
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-10-16 12:06:52 +0200

    Update print_unsigned_int.c

--- print_unsigned_int.c
+++ print_unsigned_int.c
@@ -2,8 +2,10 @@
 
 /**
  * print_unsigned_int - prints unsigned integer
- *
- *
- *
+ * @count:  a pointer to an int that keeps count
+ * @ptr: pointer
  */
 
+void print_unsigned_int(unsigned int *count, va_list *ptr)
+{
+	
