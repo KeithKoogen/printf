7d8ab167a095f104fc2517ebc496f58c58418515
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-10-16 22:38:38 +0200

    Update print_string.c

--- print_string.c
+++ print_string.c
@@ -14,9 +14,15 @@ void print_string(va_list *ptr, unsigned int *count)
    
 	i = 0;
 	str = va_arg(*ptr, char *);
-	  
-	if (0 > i < 32 || 32 >= 127)
- 	 {
-  	  printc("\x", 16, 0);
- 	 }
+	
+	for (i = 0; i; i++)
+	{
+		if (i > 0 && (i < 32 || i >= 127))
+		{
+			_puts("\\x");
+			count += 2;
+			convert(i , 16, 0);
+		}
+		printc(i, count);
+	}
 }
