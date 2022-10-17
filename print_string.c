c1bd415603a914af13490c5d78b85211033779ca
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-10-17 14:06:56 +0200

    Update print_string.c

--- print_string.c
+++ print_string.c
@@ -32,3 +32,25 @@ void print_string(va_list *ptr, unsigned int *count)
 		printc(i, count);
 	}
 }
+
+/**
+ * print_string - prints a string
+ * @ptr: pointer
+ * @count: a pointer to an int that keeps count
+ * Return: nothing
+ */
+
+void print_string(va_list *ptr, unsigned int *count)
+{
+	char *str;
+	int i;
+
+	i = 0;
+	str = va_arg(*ptr, char *);
+
+	while (str[i] != '\0')
+	{
+		printc(str[i], count);
+		++i;
+	}
+}
