d035900eae3d06b403e8311cbc8d168d594362c6
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-18 09:49:25 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -46,28 +46,6 @@ void print_integer(va_list *ptr, unsigned int *count)
 }
 
 /**
- * print_string - prints a string
- * @ptr: pointer
- * @count: a pointer to an int that keeps count
- * Return: nothing
- */
-
-void print_string(va_list *ptr, unsigned int *count)
-{
-	char *str;
-	int i;
-
-	i = 0;
-	str = va_arg(*ptr, char *);
-
-	while (str[i] != '\0')
-	{
-		printc(str[i], count);
-		++i;
-	}
-}
-
-/**
 * print_everything - prints everything
 * @format: a pointer
 * @count: pointer to a counter variable
