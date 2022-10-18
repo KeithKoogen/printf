bf5fc1b14cd6a388a0172a98da18cbb2dc1d2290
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-18 15:14:25 +0200

    Update print_reverse.c

--- print_reverse.c
+++ print_reverse.c
@@ -1,15 +1,28 @@
 #include "main.h"
 
-
+void string_printer(char *str, unsigned int *count)
+{
+	if (*str == '\0')
+		return;
+	
+	string_printer(*(str - 1));
+	printc(*str, count);
+}
 /**
- * print_reverse - prints numbers
- * @n: amount of numbers
+ * print_string - prints a string
+ * @ptr: pointer
  * @count: a pointer to an int that keeps count
  * Return: nothing
  */
 
-void print_string(va_list *ptr, unsigned int *count)
+void print_reverse(va_list *ptr, unsigned int *count)
 {
+	
+	char *str;
+	
+	str = va_arg(*ptr, char *);
+	
+	string_printer(str, count);
 
 
 }
