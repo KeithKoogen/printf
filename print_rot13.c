2e6173656b753bdee397d7855441623b65331a28
Damien Bernstein <111013117+DamienBernstein@users.noreply.github.com>
2022-10-18 16:17:34 +0200

    Update print_rot13.c

--- print_rot13.c
+++ print_rot13.c
@@ -8,14 +8,17 @@
 
 void print_rot13(va_list *ptr,char *)
 {
-int i = 0;
-int j = 0;
+int i;
+int j;
+char *str;
 char *l = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
 char *r13 = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
 
+j = 0;
+i = 0; 
 str = va_arg(*ptr, char *);
  
-while (*(s + i) != '\0')
+while (*(str + i) != '\0')
 {
 while (*(l + j) != '\0')
 {
