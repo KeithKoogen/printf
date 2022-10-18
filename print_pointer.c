f22d4643867143a09dbe49a629db23b89572f351
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-18 16:33:20 +0200

    Update print_pointer.c

--- print_pointer.c
+++ print_pointer.c
@@ -6,17 +6,17 @@
  * @ptr: pointer
  * @count: pointer to an int that keeps count  a pointer
  */
- 
- 
+
+
 void print_pointer(va_list *ptr, unsigned int *count)
 {
 	void *address;
 	uintptr_t str;
-	
+
 	address = va_arg(*ptr, void *);
 	 str = (uintptr_t)&address;
-	
-	
+
+
 	printf("%x\n", str);
 	*count = *count + 1;
 
