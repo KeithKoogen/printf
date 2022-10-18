4e2ef50bf929acbf884a716bcab3c17df88eebf3
Keith Koogen Moodley <105937078+KeithKoogen@users.noreply.github.com>
2022-10-18 16:26:31 +0200

    Update print_rot13.c

--- print_rot13.c
+++ print_rot13.c
@@ -2,7 +2,8 @@
 
 /**
  * print_rot13 - function that encodes a string using rot13.
- * @s: pointer to char
+ * @ptr: pointer
+ * @count: pointer to counter variable
  * Return: char
  */
 
@@ -15,9 +16,8 @@ char *l = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
 char *r13 = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
 
 j = 0;
-i = 0; 
+i = 0;
 str = va_arg(*ptr, char *);
- 
 while (*(str + i) != '\0')
 {
 while (*(l + j) != '\0')
